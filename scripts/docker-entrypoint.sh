39051114165e04adb0a98e96fc68ec9d61024856
reeen <rene@windegger.wtf>
2016-07-16 09:44:30 +0200

    Fixed the entrypoint.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -19,6 +19,6 @@ if [ "$socketMissing" = 1 -a "$1" = forego -a "$2" = start -a "$3" = '-r' ]; the
 	exit 1
 fi
 
-docker-gen /opt/scripts/varnish.tmpl /etc/varnish/varnish.vcl
+docker-gen /opt/scripts/varnish.tmpl /etc/varnish/default.vcl
 
 exec "$@"
